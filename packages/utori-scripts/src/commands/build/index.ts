3b33aeb7cbc323459fedbc48b288fe16faeb933d
Michelle Miller <chellimiller@users.noreply.github.com>
2021-05-09 16:17:55 -0700

    #2 Update types for commands

--- packages/utori-scripts/src/commands/build/index.ts
+++ packages/utori-scripts/src/commands/build/index.ts
@@ -1,16 +1,16 @@
 import { CommandModule } from 'yargs';
+import { ProjectConfig } from '../../types/ProjectConfig';
 
 /**
  * Command to build the project managed by `utori-scripts`.
  * 
  * @todo #3 Implement this for `react-web-app`.
  */
-const build: CommandModule = {
+const build: CommandModule<{}, ProjectConfig> = {
   command: 'build',
   describe: 'Builds the project for production',
-  handler: (args) => {
-    console.log(args)
-    // throw new Error('This command is unsupported! See https://github.com/utori-dev/utori-scripts/issues/3 for status.');
+  handler: () => {
+    throw new Error('This command is unsupported! See https://github.com/utori-dev/utori-scripts/issues/3 for status.');
   }
 }
 
