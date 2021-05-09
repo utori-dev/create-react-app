8f52a9b3d29f49539b60c3237857518be7913d2f
Michelle Miller <chellimiller@users.noreply.github.com>
2021-05-09 16:30:41 -0700

    #1 Add JSDocs for options

--- packages/utori-scripts/src/options/projectType/index.ts
+++ packages/utori-scripts/src/options/projectType/index.ts
@@ -1,6 +1,10 @@
 import { Options } from 'yargs';
 import { ProjectType } from '../../types/ProjectConfig';
 
+/**
+ * Option that allows the user to specify the project type.
+ * This is read in from the configuration file by default.
+ */
 const projectType: Options = {
   choices: Object.values(ProjectType),
   demandOption: true,
