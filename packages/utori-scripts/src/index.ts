e798a126b1369506342af7acbdfe565a2ebefe29
Michelle Miller <chellimiller@users.noreply.github.com>
2021-05-09 15:19:58 -0700

    #2 Add config option

--- packages/utori-scripts/src/index.ts
+++ packages/utori-scripts/src/index.ts
@@ -5,17 +5,24 @@ import build from './commands/build';
 import lint from './commands/lint';
 import start from './commands/start';
 import test from './commands/test';
+import readConfig from './util/readProjectConfig';
 
 // Was getting some errors about `scriptName` and such being undefined.
 // Tried a bunch of things, but this was all that worked.
 (require('yargs') as typeof yargs)
   .scriptName('utori-scripts')
   .usage('$0 <cmd> [args]')
+  .config('config', readConfig)
+  .option('config', {
+    alias: 'c',
+    default: 'utori.conf.json',
+    type: 'string',
+  })
   .command(build)
   .command(lint)
   .command(start)
   .command(test)
-  .strict(true)
+  .strictCommands(true)
   .demandCommand(1, 'You must specify at least one command')
   .help()
   .argv
