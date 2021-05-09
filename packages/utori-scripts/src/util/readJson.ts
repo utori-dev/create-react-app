7654ab2e59e65960b2b24fbb2d9f4d507d716ec8
Michelle Miller <chellimiller@users.noreply.github.com>
2021-05-09 14:31:09 -0700

    #2 Convert utils to synchronous (as required by yargs)

--- packages/utori-scripts/src/util/readJson.ts
+++ packages/utori-scripts/src/util/readJson.ts
@@ -1,37 +1,35 @@
-import { Result, ResultType, SimpleResult } from '../types/Result';
+import { ResultType, SimpleResult } from '../types/Result';
 import parseJson, { ParseJsonOptions } from './parseJson';
 import readFile, { ReadFileOptions } from './readFile';
 
 type ReadJsonOptions<T = object> = Partial<ReadFileOptions> & ParseJsonOptions<T>;
+type ReadJsonResult<T = any> = SimpleResult<T>;
 
-type ReadJsonError = NodeJS.ErrnoException | Error;
-type ReadJsonResult<T = any> = Result<T, ReadJsonError>;
-
-function readJson<T = any>(path: string, options: ReadJsonOptions<T>): Promise<ReadJsonResult<T>> {
+function readJson<T = any>(path: string, options: ReadJsonOptions<T> = {}): ReadJsonResult<T> {
   const { encoding, flag, validate, reviver } = options;
   const readFileOptions: ReadFileOptions | undefined = encoding ? { encoding, flag } : undefined;
 
-  return readFile(path, readFileOptions).then(
-    result => {
-      switch (result.type) {
-        case ResultType.ERROR:
-        case ResultType.UNKNOWN_ERROR:
-          return result as ReadJsonResult<T>;
-        case ResultType.DATA:
-        // The warning types should not occur with readFile. If they do, they still provide data.
-        case ResultType.WARNING:
-        case ResultType.UNKNOWN_WARNING:
-        // No type means ResultType.DATA
-        default:
-          return parseJson(result.data, { validate, reviver });
-      }
-    },
-    error => ({
+  try {
+    const result = readFile(path, readFileOptions);
+
+    if (result.type === ResultType.UNKNOWN_ERROR) return result as ReadJsonResult<T>;
+
+    try {
+      return parseJson(result.data, { validate, reviver });
+    } catch (error) {
+      return {
+        type: ResultType.UNKNOWN_ERROR,
+        message: `Unknown error occurred while parse '${result.data}'`,
+        error,
+      };
+    }
+  } catch (error) {
+    return {
       type: ResultType.UNKNOWN_ERROR,
       message: `Unknown error occurred while attempting to read file '${path}'`,
       error,
-    })
-  );
+    };
+  }
 }
 
 export default readJson;
