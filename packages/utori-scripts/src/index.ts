d1934aecb6c3e48e62b97857e2064580d9a49948
Michelle Miller <chellimiller@users.noreply.github.com>
2021-05-09 11:31:44 -0700

    #1 Add utori-scripts to react-web-app example

--- packages/utori-scripts/src/index.ts
+++ packages/utori-scripts/src/index.ts
@@ -1,18 +1,21 @@
 #!/usr/bin/env node
 
-import yargs from 'yargs';
+import YARGS from 'yargs';
 
-yargs
+(require('yargs') as typeof YARGS)
   .scriptName('utori-scripts')
   .usage('$0 <cmd> [args]')
-  .command('hello [name]', 'welcome ter yargs!', (yargs) => {
-    yargs.positional('name', {
-      type: 'string',
-      default: 'Cambi',
-      describe: 'the name to say hello to'
-    })
-  }, (argv) => {
-    console.log('hello', argv.name, 'welcome to yargs!')
-  })
+  .command('start [name]', 'welcome ter yargs!',
+    (yargs) => {
+      yargs.positional('name', {
+        type: 'string',
+        default: 'Cambi',
+        describe: 'the name to say hello to'
+      })
+    },
+    (argv) => {
+      console.log('start', 'welcome to yargs!')
+    },
+  )
   .help()
   .argv
