9820fcc6c4b2263a6a8f994be85b8d7af3ef605e
Michelle Miller <chellimiller@users.noreply.github.com>
2021-05-09 16:33:36 -0700

    #2 Add JSDocs for ProjectConfig

--- packages/utori-scripts/src/types/ProjectConfig.ts
+++ packages/utori-scripts/src/types/ProjectConfig.ts
@@ -1,9 +1,31 @@
+/**
+ * Type of project managed by `utori-scripts` CLI.
+ */
 export enum ProjectType {
+  /**
+   * Specifies that the project is a web app written with React.
+   */
   REACT_WEB_APP = 'react-web-app',
 }
 
+/**
+ * Configuration for the project.
+ * This is used to configure how the commands are run.
+ */
 export type ProjectConfig = {
+
+  /**
+   * Type of project being managed.
+   */
   projectType: ProjectType;
+
+  /**
+   * Directory where the source files are located.
+   */
   sourceDirectory: string;
+
+  /**
+   * Directory where the project should be built.
+   */
   outputDirectory: string;
 }
